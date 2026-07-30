73c764f351cda3667062e98b3baba60dafc1a985
Amrit Rijal <rijalamrit720@gmail.com>
2026-07-30 11:28:35 +0545

    fix: update CSS version in app layout to ensure proper styling

--- resources/views/front/layout/app.blade.php
+++ resources/views/front/layout/app.blade.php
@@ -14,7 +14,7 @@
     @if (!View::hasSection('meta'))
         @includeIf('front.cache.meta')
     @endif
-    <link rel="stylesheet" href="{{ asset('asset/front/css/index.css') }}?v=1">
+    <link rel="stylesheet" href="{{ asset('asset/front/css/index.css') }}?v=1.3">
 
     @yield('style')
 
